2cfbe0adae58be6451420b18c6656d7b27d1387f
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-09-30 14:36:18 -0400

    initialisation projet spring angular

---
+++ src/test/java/com/lina/everydayapp/EverydayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lina.everydayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
